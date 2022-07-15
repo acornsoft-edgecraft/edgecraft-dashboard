b34a6a054912de9423a5995d4bc0bbb3f481c593
ccambomorris <ccambo@acornsoft.io>
2022-07-15 19:45:49 +0900

    add stepper component

---
+++ src/components/k3/step-view.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="component-container card">
+        <div class="step-container">
+            <K3Steps :model="steps"
+                     :readonly="readonly" />
+        </div>
+        <router-view v-slot="{ Component }"
+                     :formData="data"
+                     @prev-page="prevPage($event)"
+                     @next-page="nextPage($event)"
+                     @complete="complete">
+            <keep-alive>
+                <component :is="Component" />
+            </keep-alive>
+        </router-view>
+    </div>
+</template>
+
+<script setup lang="ts">
+const router = useRouter()
+
+const props = defineProps({
+    steps: { type: Array<any>, default: null },
+    data: { type: Object, default: null },
+    readonly: { type: Boolean, default: true },
+    exact: { type: Boolean, default: true }
+})
+
+const prevPage = (event) => { }
+const nextPage = (event) => { }
+const complete = () => { }
+
+</script>
+
+<style scoped lang="scss">
+</style>
