7f06ddc6010b6641e4abafe758374eea2a052a91
ccambomorris <ccambo@acornsoft.io>
2022-09-01 14:30:59 +0900

    modify input-field to IME mode

--- src/components/k3/form/input-field.vue
+++ src/components/k3/form/input-field.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="item-container">
         <div class="form-field">
-            <K3InputText v-model="modelValue.$model"
+            <K3InputText :value="modelValue.$model"
                          :type="type"
-                         :class="itemClass" />
+                         :class="itemClass"
+                         @input="changeValue"
+                         :autofocus="autofocus" />
         </div>
         <div class="error-field basic label pointing error"
              v-if="modelValue.$invalid">{{ errorMessages }}</div>
@@ -15,12 +17,17 @@ const props = defineProps({
     modelValue: { type: Object, default: () => { } },
     type: { type: String, default: "text" },
     fieldName: { type: String, default: "Field" },
+    autofocus: { type: Boolean, default: false }
 });
-const itemClass = computed(() => [useAttrs().class /*{ 'p-invalid': props.modelValue.$invalid } */]);
+const itemClass = computed(() => [useAttrs().class, /*{ 'p-invalid': props.modelValue.$invalid } */]);
 const errorMessages = computed(() => {
     props.modelValue.$touch();
     return props.modelValue.$errors.map((err) => err.$message.replace("Value", props.fieldName)).join(",");
 });
+
+const changeValue = (e) => {
+    props.modelValue.$model = e.target.value
+}
 </script>
 
 <style scoped lang="scss">
