28c708e5be54fad4b4b4ca227ec24a31e623a4d0
ccambomorris <ccambo@acornsoft.io>
2022-09-02 15:50:13 +0900

    upgrade @vueuse and module setting to nuxt.config

--- src/composables/useAppHelper.ts
+++ src/composables/useAppHelper.ts
@@ -1,6 +1,6 @@
 import { useRuntimeConfig } from '#app'
 import { join } from 'pathe'
-import { useFetch, useClipboard, usePermission, useStorage, StorageSerializers, isNumber } from '@vueuse/core'
+import { useFetch, useClipboard, usePermission, useStorage, StorageSerializers } from '@VueUse/core'
 import { FilterMatchMode } from "primevue/api";
 import { useToast } from 'primevue/usetoast'
 import { useConfirm } from 'primevue/useconfirm'
