28acc1786c7dad628cb54f331b5c27abd1aa35ee
sunmi <sunmi@acornsoft.io>
2022-10-17 15:58:28 +0900

    add chageValue for textarea-field component

--- src/components/k3/form/textarea-field.vue
+++ src/components/k3/form/textarea-field.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="item-container">
     <div class="form-field">
-      <K3Textarea :value="modelValue.$model" :type="type" :rows="rows" :cols="cols" :class="itemClass" />
+      <K3Textarea :value="modelValue.$model" :type="type" :rows="rows" :cols="cols" :class="itemClass" @input="changeValue" />
     </div>
     <div class="error-field basic label pointing error" v-if="modelValue.$invalid">{{ errorMessages }}</div>
   </div>
@@ -21,6 +21,10 @@ const errorMessages = computed(() => {
   props.modelValue.$touch();
   return props.modelValue.$errors.map((err) => err.$message.replace(/Value|The value|This field/g, props.fieldName)).join(",");
 });
+
+const changeValue = (e) => {
+  props.modelValue.$model = e.target.value;
+};
 </script>
 
 <style scoped lang="scss"></style>
