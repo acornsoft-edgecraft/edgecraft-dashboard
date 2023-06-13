afb38996db7f45c49ba32ca5d2ec9a1e02e8818b
dongmook <dongmook@acornsoft.io>
2023-06-13 14:38:56 +0900

    update Dockerfile for nuxt framwork

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # edgecraft-dashboard
 
 
+## docker build
+
+- backend uri 환경변수를 정의 해야 한다
+
+```sh
+## default env
+# ENV BACKEND_BASE_URL=http://192.168.88.201
+# ENV BACKEND_BASE_PORT=31810
+
+$ docker build -e BACKEND_BASE_URL=http://192.168.88.201 -e BACKEND_BASE_PORT=31810 --tag edgecraft-dashboard .
+```
+
 **--Update Required--**
 
 
---
+++ scripts/shell-scripts/change_backend_uri.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -x
+set -eE
+
+index=0
+scheme=""
+url=""
+for i in $(echo $BACKEND_BASE_URL | tr "://" "\n")
+do
+    index=$((index + 1))
+    if [ $index == 1 ]; then
+        scheme=$i
+    else
+        url=$i
+    fi
+done
+
+check_str="\"production\""
+change_str="backendUrl\:\"$scheme\:\\\u002F\\\u002F$url\"\,backendPort\:\"$BACKEND_BASE_PORT\""
+change_str2="\"backendUrl\"\:\"$scheme\:\/\/$url\"\,\"backendPort\"\:\"$BACKEND_BASE_PORT\""
+
+public_list=$(grep -l -r -i "$check_str" public)
+server_list=$(grep -l -r -i "$check_str" server)
+
+for i in $public_list
+do
+    sed -i -e "s/\($check_str\).*\(version\)/\1\,$change_str\,\2/" $i
+done
+
+for i in $server_list
+do
+    sed -i -e "s/\(\(NUXT_\).*\($check_str\)\).*\(\(\"version\).*\(public\)\)/\1\,$change_str2\,\4/" $i
+    sed -i -e "s/\(\(public\).*\($check_str\)\).*\(\"version\)/\1\,$change_str2\,\4/" $i
+done
